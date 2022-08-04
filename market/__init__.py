83127aecdafa1c2ab3a1783bf7e5a4f722ccfd4a
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-04 19:35:57 +0300

    Added Register and Form class

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '7b04e086374e711904c47e40'
+
 db = SQLAlchemy(app)
 
 from market import routes
