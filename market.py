d9a716b843bada7dd266451b12d1c84d4ac04083
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-04 14:14:48 +0300

    Connected Bootstrap, added Navbar

--- market.py
+++ market.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
 
-# Refer to local .py file
 app = Flask(__name__)
 
-# Routes
 @app.route("/")
+@app.route("/home")
 def home_page():
     return render_template('home.html')
