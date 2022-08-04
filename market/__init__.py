b3403ba7f57426bcbd793750ecc756e838d5921f
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-04 16:12:33 +0300

    Added SQLite Database, Organized Files, Packages

---
+++ market/__init__.py
@@ -0,0 +1,10 @@
+from enum import unique
+from operator import length_hint
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+from market import routes
